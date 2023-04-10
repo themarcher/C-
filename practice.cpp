619a6c3136574f90f0fe6b6a130a588f3f61d03a
themarcher <123203697+themarcher@users.noreply.github.com>
2023-04-10 16:56:50 +0600

    Create practice.cpp

---
+++ practice.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    int a,b,c;
+    cin>>a>>b;
+    if((a==1 && b==0)||(a==0 || b==1)||(a==1 || b==1)){
+        c==1;
+        cout<<"c="<<c;
+    }
+    else
+    {
+        c==0;
+        cout<<"c="<<c;
+    }
+}
